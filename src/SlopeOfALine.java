376dc128e42ce1f3fdb28fecb090d6f41bf803cf
levybe <levybe@ssfs.org>
2021-10-21 10:51:58 -0400

    Finished SlopeOfALine, fixed SlopeTests.

--- src/SlopeOfALine.java
+++ src/SlopeOfALine.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class SlopeOfALine {
 
     /**
@@ -9,13 +10,28 @@ public class SlopeOfALine {
      * @return - Different strings based on whether the line is vertical or not
      */
     public static String slope(int x1, int y1, int x2, int y2) {
-
+        double slope = (double) (y2 - y1) / (x2 - x1);
         // update the line below to return a properly formatted string
-        return "";
+        if (x2 - x1 != 0) {
+            return "The line defined by the points (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") has a slope of " + slope;
+        }
+        else {
+            return "The line defined by the points (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ") is a vertical line and the slope is undefined";
+        }
     }
 
     public static void main(String[] args) {
         // Get input from the user (you will need to create a Scanner object)
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter x1:");
+        int x1 = scan.nextInt();
+        System.out.println("Enter y1:");
+        int y1 = scan.nextInt();
+        System.out.println("Enter x2:");
+        int x2 = scan.nextInt();
+        System.out.println("Enter y2:");
+        int y2 = scan.nextInt();
         // Call and print the result of the slope method as shown in the instructions.
+        System.out.println(slope(x1, y1, x2, y2));
     }
 }
