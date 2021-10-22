07bda1408df37c16ec2d75d35c07fae8ffdd2905
levybe <levybe@ssfs.org>
2021-10-22 11:55:23 -0400

    Finished DiscriminantAndRoots, started work on Palindrome.

--- src/DiscriminantAndRoots.java
+++ src/DiscriminantAndRoots.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 public class DiscriminantAndRoots {
 
     // Create the method roots in the space below.
     public static String roots(int a, int b, int c) {
-
-        return ""; // update this line so that it returns properly formatted output
+        double number = (b * b) - 4 * a * c;
+        if (number > 0) {
+            return a + "x^2 + " + b + "x + " + c + " has 2 real roots";
+        }
+        else if (number == 0) {
+            return a + "x^2 + " + b + "x + " + c + " has 1 real root";
+        }
+        else {
+            return a + "x^2 + " + b + "x + " + c + " has 2 imaginary roots";
+        }
+    }
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b, c;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        c = scan.nextInt();
+        System.out.println(roots(a, b, c));
     }
 
 }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
 public class Palindrome {
 
     // Write your isPalindrome method in the space below. You do not need to write a main method for this program
     // Your method should be static.
+    public static boolean isPalindrome(int number) {
+        int num1 = (number % 10)
+    }
 
 }
