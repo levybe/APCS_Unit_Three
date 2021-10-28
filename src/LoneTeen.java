1dea0eac39266b47d31a06e03784d7c187986a66
levybe <levybe@ssfs.org>
2021-10-28 09:00:32 -0400

    Finished LoneTeen, started Season.

--- src/LoneTeen.java
+++ src/LoneTeen.java
@@ -2,4 +2,15 @@ public class LoneTeen {
 
     // In the space below, write the loneTeen method. Make sure the signature matches
     // the one given in the instructions.
+    public static boolean loneTeen (int int1, int int2) {
+        if ((int1 >= 13 && int1 <= 19) && (int2 >= 13 && int2 <= 19)) {
+            return false;
+        }
+        else if ((int1 >= 13 && int1 <= 19) || (int2 >= 13 && int2 <= 19)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 }
--- src/PigLatin.java
+++ src/PigLatin.java
@@ -4,5 +4,7 @@ public class PigLatin {
         String letter1 = word.substring(0, 1);
         letter1 = letter1.toUpperCase();
         if (word.substring(0, 1) == "A" || word.substring(0, 1) == "E" || word.substring(0, 1) == "I" || word.substring(0, 1) == "O" || word.substring(0, 1) == "U")
+    return "hi";
+        else return "hi";
     }
 }
